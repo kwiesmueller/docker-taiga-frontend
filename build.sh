cc1bdda34f3ffea684bbdb2bf482196b6d579a3c
Kevin Wiesmüller <kwiesmueller@seibert-media.net>
2017-01-31 19:13:03 +0100

    Fix Tag

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-set -e
+CONTENT_LENGTH="$(stat -f%z cloudbuild.json)"
+ACCESS_TOKEN="$(gcloud auth print-access-token)"
 
-make build
-make upload
+curl -v -X POST -T cloudbuild.json -H "Content-Type: application/json" \
+                                             -H "Content-length: ${CONTENT_LENGTH}" \
+                                             -H "Authorization: Bearer ${ACCESS_TOKEN}" \
+                                             https://cloudbuild.googleapis.com/v1/projects/playnet-gce/builds
